4ea6e8d525d8332275f0dccf37901c6548529d1b
Jorge <93343250+jsuazof@users.noreply.github.com>
2022-02-07 13:50:17 -0300

    final
    final

---
+++ js/clientes.js
@@ -0,0 +1,19 @@
+export class Cliente {
+    constructor(nombre, impuesto) {
+        this._nombre = nombre;
+        this._impuesto = impuesto;
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+    set nombre(value) {
+        this._nombre = value;
+    }
+
+    calcularImpuesto() {
+        return (
+            (this._impuesto.montoBrutoAnual - this._impuesto.deducciones) * 0.21
+        );
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,13 @@
+import {Cliente} from "./clientes.js";
+import {Impuesto} from "./impuesto.js";
+
+const impuestoUno = new Impuesto()
+
+impuestoUno.montoBrutoAnual = 222222222
+impuestoUno.deducciones = 4000
+
+const cliente1 = new Cliente (ClientesUno, impuestoUno)
+const resultadoClientes1 = cliente1.calcularImpuesto()
+console.log(resultadoCliente1);
+
+
