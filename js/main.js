1a223280c40440656540fb3b4b7158f3beebcec7
Jorge <93343250+jsuazof@users.noreply.github.com>
2022-02-07 14:51:13 -0300

    update main.js
    update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import { Cliente } from "./cliente.js";
 import { Impuesto } from "./impuesto.js";
 
-const impuestoUno = new Impuesto();
-impuestoUno.montoBrutoAnual = 1000000;
-impuestoUno.deducciones = 2000;
+const impuestoA = new Impuesto();
+impuestoA.montoBrutoAnual = 222222222;
+impuestoA.deducciones = 4444;
 
-const cliente1 = new Cliente("ClienteUno", impuestoUno);
-const resultadoCliente1 = cliente1.calcularImpuesto();
-console.log(resultadoCliente1);
+const clienteA = new Cliente("Cliente-A", impuestoA);
+const totalCliente = clienteA.calcularImpuesto();
+console.log(totalCliente);
